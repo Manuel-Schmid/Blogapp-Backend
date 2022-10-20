420822ccc8083489c7905fd28f8cfc3abb1be073
Manuel Schmid <70013637+Manuel-Schmid@users.noreply.github.com>
2022-10-20 15:05:26 +0200

    create django ModelForms

---
+++ blog/forms.py
@@ -0,0 +1,38 @@
+from django.forms import ModelForm
+from blog.models import User, Category, Post, Comment, PostLike
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'username', 'first_name', 'last_name']
+
+
+class CategoryForm(ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'text', 'category', 'owner']
+
+
+class PostLikeForm(ModelForm):
+    class Meta:
+        model = PostLike
+        fields = ['post', 'user']
+
+
+class CreateCommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['title', 'text', 'post', 'owner']
+
+
+class UpdateCommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['title', 'text']
