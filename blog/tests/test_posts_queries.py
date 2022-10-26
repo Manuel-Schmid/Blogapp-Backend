591bea093ef24d6d7ff2a8958cf8647f4b760a61
Manuel Schmid <70013637+Manuel-Schmid@users.noreply.github.com>
2022-10-26 08:26:55 +0200

    add post-filter-query tests pt. II

--- blog/tests/test_posts_queries.py
+++ blog/tests/test_posts_queries.py
@@ -97,3 +97,60 @@ def test_query_posts_by_tag(
     post1_title = posts[0].get('title', None)
     assert post1_title is not None
     assert post1_title == 'Test_Post 2'
+
+
+@pytest.mark.django_db(transaction=True, reset_sequences=True)
+def test_query_existing_post_by_tag_and_category(
+    create_tags: Callable,
+    import_query: Callable,
+    client_query: Callable,
+) -> None:
+    create_tags()
+    filter_input = {"tagSlugs": "tag_2_slug", "categorySlug": "test_category2"}
+
+    query: str = import_query('paginatedFilteredPostsQuery.graphql')
+    response: Response = client_query(query, filter_input)
+
+    assert response is not None
+    assert response.errors is None
+
+    paginated_posts: Dict = response.data.get('paginatedPosts', None)
+    assert paginated_posts is not None
+
+    num_post_pages: Dict = paginated_posts.get('numPostPages', None)
+    assert num_post_pages is not None
+    assert num_post_pages == 1
+
+    posts: Dict = paginated_posts.get('posts', None)
+    assert posts is not None
+    assert len(posts) == 1
+    post1_title = posts[0].get('title', None)
+    assert post1_title is not None
+    assert post1_title == 'Test_Post 2'
+
+
+@pytest.mark.django_db(transaction=True, reset_sequences=True)
+def test_query_non_existent_post_by_tag_and_category(
+    create_tags: Callable,
+    import_query: Callable,
+    client_query: Callable,
+) -> None:
+    create_tags()
+    filter_input = {"tagSlugs": "tag_2_slug", "categorySlug": "test_category1"}
+
+    query: str = import_query('paginatedFilteredPostsQuery.graphql')
+    response: Response = client_query(query, filter_input)
+
+    assert response is not None
+    assert response.errors is None
+
+    paginated_posts: Dict = response.data.get('paginatedPosts', None)
+    assert paginated_posts is not None
+
+    num_post_pages: Dict = paginated_posts.get('numPostPages', None)
+    assert num_post_pages is not None
+    assert num_post_pages == 1
+
+    posts: Dict = paginated_posts.get('posts', None)
+    assert posts is not None
+    assert len(posts) == 0
