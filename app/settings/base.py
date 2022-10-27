f1a7117081f101748215e5c3391d35750fdb8155
Manuel Schmid <70013637+Manuel-Schmid@users.noreply.github.com>
2022-10-27 16:52:17 +0200

    MBP-064: remove sectret_key from env vars

--- app/settings/base.py
+++ app/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-7lmcz^4__&y%40#mn=l@u!xqo=npd*50sht^h905=khcepik0f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
