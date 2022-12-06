06d9d898e88009e788f6cbcba556adcc476ff8de
Manuel Schmid <70013637+Manuel-Schmid@users.noreply.github.com>
2022-12-06 15:17:45 +0100

    MBP-086: UserStatus is updated when accepting Author Request in Django Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.html import format_html
+
+from blog.api.inputs import Status
 from blog.models import (
     Comment,
     User,
@@ -9,6 +12,7 @@ from blog.models import (
     CommentLike,
     PostLike,
     AuthorRequest,
+    UserStatus,
 )
 
 admin.site.register(User, UserAdmin)
@@ -24,8 +28,27 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'date_created', 'category', 'owner', 'image_tag')
 
 
+class AuthorRequestAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date_opened', 'date_closed', 'status')
+
+    def save_model(self, request, obj, form, change):
+        field = 'status'
+        super().save_model(request, obj, form, change)
+        if change and field in form.changed_data and form.cleaned_data.get(field):
+            new_status = form.cleaned_data.get(field)
+            if new_status == Status.PENDING.name:
+                form.instance.date_closed = None
+            else:
+                form.instance.date_closed = datetime.now()
+            form.save()
+
+            user_status = UserStatus.objects.get(user=form.data.get('user'))
+            user_status.is_author = new_status == Status.ACCEPTED.name
+            user_status.save()
+
+
 admin.site.register(Post, PostAdmin)
-admin.site.register(AuthorRequest)
+admin.site.register(AuthorRequest, AuthorRequestAdmin)
 
 admin.site.register(Comment)
 admin.site.register(CommentLike)
